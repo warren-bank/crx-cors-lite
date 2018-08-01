0c6aa28508b5c3addea38e79af863ef70fca55c0
Warren R Bank <warren.r.bank@gmail.com>
2018-08-01 00:22:00 -0700

    add ability to remove response headers

--- CORS-Lite/background.js
+++ CORS-Lite/background.js
@@ -1,47 +1,72 @@
-var url_regex_pattern
+var regex_patterns = {
+  url:     null,
+  headers: null
+}
 
-var update_url_regex_pattern = function(str) {
-  var backup = url_regex_pattern
-  try {
-    url_regex_pattern = new RegExp(str, 'i')
+var update_regex_pattern = function(key, str) {
+  var backup
+
+  if (!str) {
+    regex_patterns[key] = null
     return true
   }
-  catch(e) {
-    url_regex_pattern = backup
-    return false
+  else {
+    backup = regex_patterns[key]
+    try {
+      regex_patterns[key] = new RegExp(str, 'i')
+      return true
+    }
+    catch(e) {
+      regex_patterns[key] = backup
+      return false
+    }
   }
 }
 
 var reload = function() {
-  chrome.storage.sync.get("url_regex_pattern", function(items) {
-    update_url_regex_pattern(items.url_regex_pattern)
+  chrome.storage.sync.get(["url_regex_pattern", "headers_regex_pattern"], function(items) {
+    var key, val
+    for (key in items) {
+      val = items[key]
+      key = key.replace('_regex_pattern', '')
+      update_regex_pattern(key, val)
+    }
   })
 }
 
 var responseListener = function(details) {
-  if (details && details.url && url_regex_pattern && url_regex_pattern.test(details.url)) {
+  var headers = (details.responseHeaders && details.responseHeaders.length) ? [...details.responseHeaders] : []
+
+  if (details && details.url && regex_patterns.url && regex_patterns.url.test(details.url)) {
     var flag = false
     var rule = {
       "name": "access-control-allow-origin",
       "value": "*"
     }
 
-    for (var i = 0; i < details.responseHeaders.length; i++) {
-      if (details.responseHeaders[i].name.toLowerCase() === rule.name) {
+    for (var i=(headers.length - 1); i>=0; i--) {
+      if (headers[i].name.toLowerCase() === rule.name) {
         flag = true
-        details.responseHeaders[i].value = rule.value
-        break
+        headers[i].value = rule.value
+      }
+      else if (regex_patterns.headers) {
+        if (regex_patterns.headers.test(headers[i].name)) {
+          headers.splice(i, 1)
+        }
       }
     }
 
-    if (!flag) details.responseHeaders.push(rule)
+    if (!flag) headers.push(rule)
   }
+
+  return {responseHeaders: headers}
 }
 
 chrome.webRequest.onHeadersReceived.addListener(
   responseListener,
   {
-    urls:["<all_urls>"]
+    urls:  ["<all_urls>"],
+    types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"]
   },
   ["blocking", "responseHeaders"]
 )
@@ -50,8 +75,23 @@ chrome.runtime.onInstalled.addListener(
   function(details){
     if (details.reason === "install"){
       // initialize default value of option(s)
+      var url_regexs, headers_regexs
+
+      url_regexs = []
+      url_regexs.push("\\.(?:mp4|mp4v|mpv|m1v|m4v|mpg|mpg2|mpeg|xvid|webm|3gp|avi|mov|mkv|ogv|ogm|m3u8|mpd|ism(?:[vc]|/manifest)?|vtt|srt|sami|dfxp)(?:[\\?#].*)?$")
+      url_regexs.push("pbs\\.org/redirect/")
+      url_regexs = "(?:" + url_regexs.join("|") + ")"
+
+      headers_regexs = []
+      headers_regexs.push("Access-Control-Max-Age")
+      headers_regexs.push("Access-Control-Allow-Methods")
+      headers_regexs.push("Access-Control-Allow-Headers")
+      headers_regexs.push("Content-Security-Policy")
+      headers_regexs = "(?:" + headers_regexs.join("|").toLowerCase() + ")"
+
       chrome.storage.sync.set({
-        "url_regex_pattern": "\\.(?:mp4|mp4v|mpv|m1v|m4v|mpg|mpg2|mpeg|xvid|webm|3gp|avi|mov|mkv|ogv|ogm|m3u8|mpd|ism(?:[vc]|/manifest)?|vtt|srt|sami|dfxp)(?:[\\?#].*)?$"
+        "url_regex_pattern":     url_regexs,
+        "headers_regex_pattern": headers_regexs
       }, reload)
     }
   }
--- CORS-Lite/options.js
+++ CORS-Lite/options.js
@@ -8,25 +8,41 @@ function show_status_message(msg) {
 
 // Saves options to chrome.storage
 function save_options() {
-  var url_regex_pattern = document.getElementById('url_regex_pattern').value
+  var error
+
+  var process_input = function(bg) {
+    ['url_regex_pattern', 'headers_regex_pattern'].forEach(function(id){
+      if (error) return
+
+      var val = document.getElementById(id).value
+      var key = id.replace('_regex_pattern', '')
+
+      if (! bg.update_regex_pattern(key, val)) {
+        error = key.toUpperCase() + ' regex pattern is invalid.'
+      }
+    })
+  }
 
   chrome.runtime.getBackgroundPage(function(bg){
-    if (bg.update_url_regex_pattern(url_regex_pattern)) {
+    process_input(bg)
+
+    if (error) {
+      show_status_message(error)
+    }
+    else {
       chrome.storage.sync.set({
-        "url_regex_pattern": url_regex_pattern
+        "url_regex_pattern":     document.getElementById("url_regex_pattern").value,
+        "headers_regex_pattern": document.getElementById("headers_regex_pattern").value
       }, function() {
         show_status_message('Options saved.')
       })
-    }
-    else {
-      show_status_message('Invalid pattern.')
     }
   })
 }
 
 // Restores select box and checkbox state using the preferences stored in chrome.storage.
 function restore_options() {
-  chrome.storage.sync.get(['url_regex_pattern'], function(items) {
+  chrome.storage.sync.get(['url_regex_pattern', 'headers_regex_pattern'], function(items) {
     for (var key in items){
       document.getElementById(key).value = items[key];
     }
